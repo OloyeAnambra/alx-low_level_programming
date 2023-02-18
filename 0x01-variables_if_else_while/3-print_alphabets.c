2dd479bde89250e802b2aa8a7c9358a3e009b64f
chisom <obedreal6@gmail.com>
2023-02-17 20:52:06 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,16 +9,20 @@
 
 int main(void)
 {
-	char c;
+	int ch = 'a';
+	int CH = 'A';
 
-	for (c = 'a' ; c <= 'z' ; c++)
+	while (ch <= 'z')
 	{
-		if ((c == 'q' || c == 'e') != 1)
-		{
-			putchar(c);
-		}
-
+		putchar(ch);
+		ch++;
 	}
+	while (CH <= 'Z')
+	{
+		putchar(CH);
+		CH++;
+	}
+	
 	putchar('\n');
 	return (0);
 }
