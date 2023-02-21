cdc491d74791807aa1d6cc690ce059874961f4b0
chisom <obedreal6@gmail.com>
2023-02-20 16:49:58 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/**
+ * main -print a c program that reurns one
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
