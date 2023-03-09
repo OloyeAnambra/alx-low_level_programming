1ea59bf363caeab0d678c3debbeff211e932cb11
chisom <obedreal6@gmail.com>
2023-03-08 16:41:42 -0800

    C - Recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * Auth: me
+ * Desc: header file containing prototypes for all functions
+ * used in 0x08-recursion directory
+ */
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+
+#endif
