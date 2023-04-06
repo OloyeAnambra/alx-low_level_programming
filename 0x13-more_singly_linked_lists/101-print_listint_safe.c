6c1380550e1d7cc9bd58f36b3f1066fbb3d7a3c4
chisom <obedreal6@gmail.com>
2023-04-05 21:41:51 -0700

    a function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,8 +1,49 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - a function that finds the loop in a linked list
- * @head: a pointer to the head of the linked list
+ * print_listint_safe - Prints a listint_t linked list.
  *
- * Return: address of the node where the loop starts (Success), NULL (Fail)
+ * @head: A pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
  */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *low, *high;
+	size_t count;
+
+	low = head;
+	high = head;
+	count = 0;
+
+	while (low != NULL && high != NULL && high->next != NULL)
+	{
+	printf("[%p] %d\n", (void *)low, low->n);
+	low = low->next;
+	high = high->next->next;
+
+	count++;
+
+	if (low == high)
+		{
+		printf("[%p] %d\n", (void *)low, low->n);
+		printf("-> [%p] %d\n", (void *)high->next, high->next->n);
+		count++;
+		break;
+		}
+	}
+
+	if (low == NULL || high == NULL || high->next == NULL)
+	{
+	while (head != NULL)
+		{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		count++;
+		}
+	}
+
+	return (count);
+}
+
